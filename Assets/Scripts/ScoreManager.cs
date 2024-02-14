1ceefdf7bb05ac2ace2c6f0d13df650e4df9ceb7
Sharri'a Sneed <frostellachery26@gmail.com>
2024-02-14 12:27:08 -0600

    Update ScoreManager.cs
    -Not Tested

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class ScoreManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class ScoreManager : MonoBehaviour{
+    //Made a reference to TMP_Text
+    [SerializeField] TMP_Text scoreText;
+    //
+    private int score = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    // Start is called before the first frame update
+    void Start(){
+        //Changes the text of scoreText
+        scoreText.text = "Score: " + score.ToString();
+    }//Closes the start method
+
+    //This method is to change the current score//
+    public void AddScore(int scoreToAdd){
+        //adds whatever the value of scoreToAdd to score
+        score += scoreToAdd;
+        //Changes the text of scoreText
+        scoreText.text = "Score: " + score.ToString();
+    }//Closes the addScore method
 }
